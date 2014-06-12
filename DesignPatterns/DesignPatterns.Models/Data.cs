df0fe4a53c06e0c551a132bc45f8c5bda45f3e34
Azmi ŞAHİN <azmisahin@outlook.com>
2014-06-12 06:30:27 +0300

    Models &  EntityFramework & CodeFirst O/RM ( UserTable  )

---
+++ DesignPatterns/DesignPatterns.Models/Data.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+namespace DesignPatterns.Models
+{
+    public partial class Data:DbContext
+    {
+        public Data()
+        {
+        }
+        public DbSet<UserTable> Users { get;set; }
+    }
+}
---
+++ DesignPatterns/DesignPatterns.Models/UserTable.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace DesignPatterns.Models
+{
+    [Table("Users")]
+    public partial class UserTable
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string Name { get; set; }
+    }
+}
