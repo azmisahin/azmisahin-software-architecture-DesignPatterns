478e30f5920ecee325da333bfc70ce155edf9f0d
Azmi ŞAHİN <azmisahin@outlook.com>
2014-10-27 22:24:59 +0200

    Structural Patterns -> Adapter

---
+++ DesignPatterns/DesignPatterns.StructuralPatterns/Adapter/Adapte.cs
@@ -0,0 +1,37 @@
+﻿using DesignPatterns.Models;
+namespace DesignPatterns.StructuralPatterns.Adapter
+{
+    #region Sample Data
+    public class AdaptorData
+    {
+        public int id { get; set; }
+        public object data { get; set; }
+    }    
+    #endregion
+    
+
+    public interface iAdapte
+    {
+        void Convert(AdaptorData model);
+    }
+    public class Adapte
+    {   void Convert(AdaptorData model)
+        {
+            var ret = model.data;
+        }
+    }
+    public class StringAdapte : iAdapte
+    {
+        public void Convert(AdaptorData model)
+        {
+            var ret = model.data.ToString();
+        }
+    }
+    public class IntAdapte : iAdapte
+    {
+        public void Convert(AdaptorData model)
+        {
+            var ret = (int)model.data;
+        }
+    }
+}
