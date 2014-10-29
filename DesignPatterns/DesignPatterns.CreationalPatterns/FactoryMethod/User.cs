4505e3faa789bc6861223aa2f4a44f463de6cdca
Azmi ŞAHİN <azmisahin@outlook.com>
2014-10-29 23:11:11 +0200

    Creatinal Patterns - Factory Method And Test

---
+++ DesignPatterns/DesignPatterns.CreationalPatterns/FactoryMethod/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPatterns.FactoryMethod
+{
+    public abstract class User
+    {
+        public int UserId { get; set; }
+    }
+
+    public abstract class Creator
+    {
+        public abstract User Factory();
+    }
+
+    public class Admin: User
+    { }
+    public class Create:Creator
+    {
+        public override User Factory()
+        {
+            return new Admin();
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns.Test/FactoryMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using DesignPatterns.CreationalPatterns.FactoryMethod;
+namespace DesignPatterns.Test
+{
+    [TestClass]
+    public class FactoryMethod
+    {
+        [TestMethod]
+        public void FactoryMethodTest()
+        {
+            Creator[] creator = new Creator[2];
+            creator[0] = new Create();
+            creator[1] = new Create();
+            
+            Assert.IsNotNull(creator);
+        }
+    }
+}
